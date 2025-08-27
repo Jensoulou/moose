1c5fa61c16f16b42e0d2acd65c183812558346ab
Jens <jens.niclaes@uclouvain.be>
2025-08-27 10:29:26 +0200

    Debunking LowerDimensional .C and .H files

---
+++ modules/porous_flow/include/kernels/PorousFlowHeatConductionLowerDimensional.h
@@ -0,0 +1,31 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "PorousFlowHeatConduction.h"
+
+/**
+ * Multiplication of PorousFlowHeatConduction by the _aperture of the fracture
+ */
+class PorousFlowHeatConductionLowerDimensional : public PorousFlowHeatConduction
+{
+public:
+  static InputParameters validParams();
+
+  PorousFlowHeatConductionLowerDimensional(const InputParameters & parameters);
+
+protected:
+  virtual Real computeQpResidual() override;
+  virtual Real computeQpJacobian() override;
+  virtual Real computeQpOffDiagJacobian(unsigned int jvar) override;
+
+  /// Fracture aperture (width)
+  const VariableValue & _aperture;
+};
